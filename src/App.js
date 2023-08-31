cb96d1ab27e89bb9d4b3ec624c4729d4e6861c07
Solai Mithelesh <86795893+iamsolai@users.noreply.github.com>
2023-08-31 20:19:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
+const fadeOut = () => {
+  const loaderWrapper = document.querySelector(".wrapper");
+  loaderWrapper.classList.add("fade");
+};
+window.addEventListener("load", fadeOut);
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
